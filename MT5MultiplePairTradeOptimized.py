8560d60e161249a9cee39880a4db7ac99a68bdd7
LucaNoel35 <86740452+LucaNoel35@users.noreply.github.com>
2025-09-12 13:17:48 +0200

    Update MT5MultiplePairTradeOptimized.py

--- MT5MultiplePairTradeOptimized.py
+++ MT5MultiplePairTradeOptimized.py
@@ -27,7 +27,7 @@ from typing import Dict, List, Optional
 # =========================
 
 # ⚠️ Move these to environment variables in production
-nombre =  62608715              
+nombre =  62414265              
 pwd = 'Sephiroth35*'
 server_name = 'OANDATMS-MT5'
 path_name = r'C:\Program Files\OANDA TMS MT5 Terminal\terminal64.exe'
