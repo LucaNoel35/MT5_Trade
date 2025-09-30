f156ecabf5a0535b6158e0f6516fe473ce56b82b
LucaNoel35 <86740452+LucaNoel35@users.noreply.github.com>
2025-09-30 14:14:10 +0200

    Update MT5MultiplePairTrade.py

--- MT5MultiplePairTrade.py
+++ MT5MultiplePairTrade.py
@@ -26,7 +26,7 @@ from typing import Dict, List, Optional
 # =========================
 
 # ⚠️ Move these to environment variables in production
-nombre =  62873946              
+nombre =  62219880              
 pwd = 'Sephiroth35*'
 server_name = 'OANDATMS-MT5'
 path_name = r'C:\Program Files\OANDA TMS MT5 Terminal\terminal64.exe'
@@ -928,4 +928,5 @@ if __name__ == "__main__":
             print(mt5.last_error())
 
     sys.exit(0)
+
 
