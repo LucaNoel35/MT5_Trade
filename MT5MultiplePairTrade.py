b6a57b9fabfbb5f7ca75dbbeb30cc1c88f302fea
LucaNoel35 <86740452+LucaNoel35@users.noreply.github.com>
2025-10-05 20:32:39 +0200

    Update MT5MultiplePairTrade.py

--- MT5MultiplePairTrade.py
+++ MT5MultiplePairTrade.py
@@ -244,7 +244,7 @@ class ConTrader:
         self.hedge = hedge
         self.hedge_b = hedge
         self.initialize = 1
-        self.initialize_origin = initialize
+        self.initialize_origin = self.initialize
         self.beginning = 1
         self.beginning_origin = self.beginning
 
@@ -939,6 +939,7 @@ if __name__ == "__main__":
             print(mt5.last_error())
 
     sys.exit(0)
+
 
 
 
