6f97191c6c828fc7924ec9552ac8832fda741e5e
LucaNoel35 <86740452+LucaNoel35@users.noreply.github.com>
2026-01-19 10:43:54 +0100

    Update MT5MultiplePairTradeOptimized.py

--- MT5MultiplePairTradeOptimized.py
+++ MT5MultiplePairTradeOptimized.py
@@ -64,10 +64,10 @@ selection_condition_buy_sell=1
 
 selection_gain_loss=1
 
-gain_plus=1
+gain_plus=2
 loss_plus=1
-gain_minus=1
-loss_minus=2
+gain_minus=2
+loss_minus=1
 
 if selection_gain_loss==1:
   gain_plus=1.5
